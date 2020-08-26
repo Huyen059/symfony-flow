c590fcda2608051d8288d9872de4b5b019900f71
chrishargan <chrishargan@gmail.com>
2020-08-26 09:28:31 +0200

    morning update

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,28 +2,47 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Ticket;
+use App\Form\CommentType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
 {
     /**
-     * @Route("/customer", name="customer")
+     * @Route("/customer", name="customer_home")
      */
     public function index()
     {
+
         return $this->render('customer/index.html.twig', [
             'controller_name' => 'CustomerController',
         ]);
+
     }
 
     /**
-     * @Route("/customer/tickets", name="customer_tickets")
+     * @Route("/customer/tickets/{id}", name="customer_tickets")
      */
-    public function customerTickets()
+    public function customerTickets(Ticket $ticket, Request $request)
     {
+        $comment = new Comment();
+        $comment->setUser($this->getUser());
+        $comment->setTicket($ticket);
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           $this->getDoctrine()->getManager()->persist($comment);
+           $this->getDoctrine()->getManager()->flush();
+
+        }
+
         return $this->render('customer/customerTickets.html.twig', [
-            'controller_name' => 'CustomerController',
+            'form' => $form->createView()
         ]);
     }
 
