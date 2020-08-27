8d14a05a26e92147d4b142cf7bb37000f6170f73
Huyen059 <samihuyen059@gmail.com>
2020-08-27 22:36:35 +0200

    done refractor agent

--- src/Controller/AgentController.php
+++ src/Controller/AgentController.php
@@ -19,7 +19,6 @@ class AgentController extends AbstractController
 
     /**
      * @Route("/agent/home", name="agent_home", methods={"GET", "POST"})
-     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
     public function index(Request $request, MailerInterface $mailer)
     {
@@ -29,28 +28,6 @@ class AgentController extends AbstractController
         $agent = $this->getUser();
         if ($agent->getRoles()[0] === self::ROLE_AGENT_SECOND_LINE) {
             $agent->setIsSecondLine(true);
-        }
-
-        // Assign the chosen ticket to this agent
-        if ($request->request->get('addToDo')) {
-            $ticket = $this->getDoctrine()->getRepository(Ticket::class)->findOneBy(['id' => $request->request->get('addToDo')]);
-            $ticket->setAgent($agent)
-                ->setStatus(Ticket::IN_PROGRESS)
-                ->setUpdatedDate(new \DateTimeImmutable());
-            $this->getDoctrine()->getManager()->persist($ticket);
-            $this->getDoctrine()->getManager()->flush();
-
-            $email = (new Email())
-                ->from('becode@test.com')
-                ->to('samihuyen059@gmail.com')
-                ->subject('Your ticket is being handled by us.')
-                ->text('Your ticket is now being handled by one of our agent.');
-            try {
-                $mailer->send($email);
-            }
-            catch (TransportExceptionInterface $e) {
-                $error = $e->getMessage();
-            }
         }
 
         // Get the tickets that match the agent (normal agent: not-escalated ticket, second-line agent: escalated ticket)
@@ -70,9 +47,43 @@ class AgentController extends AbstractController
     }
 
     /**
-     * @Route("/agent/ticket/{id}", name="agent_ticket", methods={"GET", "POST"})
+     * @Route("/agent/addTodo", name="agent_addTodo", methods={"POST"})
      */
-    public function addComment(Ticket $ticket, Request $request, MailerInterface $mailer)
+    public function addTodo(Request $request, MailerInterface $mailer)
+    {
+        /**
+         * @var Agent $agent
+         */
+        $agent = $this->getUser();
+        if ($agent->getRoles()[0] === self::ROLE_AGENT_SECOND_LINE) {
+            $agent->setIsSecondLine(true);
+        }
+
+        $ticket = $this->getDoctrine()->getRepository(Ticket::class)->findOneBy(['id' => $request->request->get('addToDo')]);
+        $ticket->setAgent($agent)
+            ->setStatus(Ticket::IN_PROGRESS)
+            ->setUpdatedDate(new \DateTimeImmutable());
+        $this->getDoctrine()->getManager()->persist($ticket);
+        $this->getDoctrine()->getManager()->flush();
+
+        $email = (new Email())
+            ->from('becode@test.com')
+            ->to('samihuyen059@gmail.com')
+            ->subject('Your ticket is being handled by us.')
+            ->text('Your ticket is now being handled by one of our agent.');
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            $error = $e->getMessage();
+        }
+
+        return $this->redirectToRoute('agent_home');
+    }
+
+    /**
+     * @Route("/agent/ticket/{id}", name="agent_ticket", methods={"GET"})
+     */
+    public function agentTicketDetail(Ticket $ticket, Request $request, MailerInterface $mailer)
     {
         $error = '';
         /**
@@ -83,91 +94,7 @@ class AgentController extends AbstractController
             $agent->setIsSecondLine(true);
         }
         $comment = new Comment();
-        $form = $this->createForm(AgentCommentType::class, $comment);
-        $form->handleRequest($request);
-
-        // If the add comment form is submitted
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Assign user and ticket to this comment
-            $comment->setUser($agent)
-                ->setTicket($ticket);
-            // Check if this comment is private, if not, change status of ticket
-            $isPrivate = $form->get('isPrivate')->getData();
-            $comment->setIsPrivate($isPrivate);
-            if (!$isPrivate) {
-                $ticket->setStatus(Ticket::WAITING_FOR_CUSTOMER_FEEDBACK)
-                    ->setUpdatedDate(new \DateTimeImmutable());
-                $this->getDoctrine()->getManager()->persist($ticket);
-
-                $email = (new Email())
-                    ->from('becode@test.com')
-                    ->to('samihuyen059@gmail.com')
-                    ->subject('Please give us your feedback')
-                    ->text('Your ticket is handled by one of our agent. Please give us your feedback.');
-                try {
-                    $mailer->send($email);
-                }
-                catch (TransportExceptionInterface $e) {
-                    $error = $e->getMessage();
-                }
-            }
-
-            $this->getDoctrine()->getManager()->persist($comment);
-            $this->getDoctrine()->getManager()->flush();
-            return $this->redirectToRoute('agent_ticket', ['id' => $ticket->getId()]);
-        }
-
-        // If the agent closes the ticket
-        if ($request->request->get('closeTicket')) {
-            foreach ($ticket->getComments() as $comment) {
-                if ($comment->getUser() instanceof Agent && !$comment->getIsPrivate()) {
-                    $ticket->setStatus(Ticket::CLOSE)
-                        ->setUpdatedDate(new \DateTimeImmutable());
-                    $this->getDoctrine()->getManager()->persist($ticket);
-                    $this->getDoctrine()->getManager()->flush();
-
-                    $email = (new Email())
-                        ->from('becode@test.com')
-                        ->to('samihuyen059@gmail.com')
-                        ->subject('Your ticket has been closed')
-                        ->text('Your ticket is handled by one of our agent. The ticket is now closed.');
-                    try {
-                        $mailer->send($email);
-                    }
-                    catch (TransportExceptionInterface $e) {
-                        $error = $e->getMessage();
-                    }
-
-                    return $this->redirectToRoute('agent_ticket', ['id' => $ticket->getId()]);
-                }
-            }
-
-            $error = "A ticket can only be closed when it has at least one public comment from an agent.";
-        }
-
-        // If the agent escalates the ticket
-        if ($request->request->get('escalateTicket')) {
-            $ticket->setIsEscalated(true)
-                ->setAgent(null)
-                ->setStatus(Ticket::OPEN)
-                ->setUpdatedDate(new \DateTimeImmutable());
-            $this->getDoctrine()->getManager()->persist($ticket);
-            $this->getDoctrine()->getManager()->flush();
-
-            $email = (new Email())
-                ->from('becode@test.com')
-                ->to('samihuyen059@gmail.com')
-                ->subject('Your ticket is escalated')
-                ->text('Your ticket will be handled by one of our second line agent.');
-            try {
-                $mailer->send($email);
-            }
-            catch (TransportExceptionInterface $e) {
-                $error = $e->getMessage();
-            }
-
-            return $this->redirectToRoute('agent_home');
-        }
+        $form = $this->createForm(AgentCommentType::class, $comment, ['action' => $this->generateUrl('agent_add_comment', ['id' => $ticket->getId()])]);
 
         return $this->render('agent/detail.html.twig', [
             'ticket' => $ticket,
@@ -182,5 +109,107 @@ class AgentController extends AbstractController
             'medium_priority' => Ticket::MEDIUM_PRIORITY,
             'low_priority' => Ticket::LOW_PRIORITY,
         ]);
+    }
+
+    /**
+     * @Route("/agent/ticket/addComment/{id}", name="agent_add_comment", methods={"POST"})
+     */
+    public function addComment(Ticket $ticket, Request $request, MailerInterface $mailer)
+    {
+        /**
+         * @var Agent $agent
+         */
+        $agent = $this->getUser();
+        if ($agent->getRoles()[0] === self::ROLE_AGENT_SECOND_LINE) {
+            $agent->setIsSecondLine(true);
+        }
+
+        $form = $request->request->get('agent_comment');
+        $comment = new Comment();
+        // Assign user and ticket to this comment
+        $comment->setUser($agent)
+            ->setTicket($ticket)
+            ->setTitle($form['title'])
+            ->setContent($form['content']);
+        // Check if this comment is private, if not, change status of ticket
+        $isPrivate = $form['isPrivate'] ? true : false;
+        $comment->setIsPrivate($isPrivate);
+        if (!$isPrivate) {
+            $ticket->setStatus(Ticket::WAITING_FOR_CUSTOMER_FEEDBACK)
+                ->setUpdatedDate(new \DateTimeImmutable());
+            $this->getDoctrine()->getManager()->persist($ticket);
+
+            $email = (new Email())
+                ->from('becode@test.com')
+                ->to('samihuyen059@gmail.com')
+                ->subject('Please give us your feedback')
+                ->text('Your ticket is handled by one of our agent. Please give us your feedback.');
+            try {
+                $mailer->send($email);
+            } catch (TransportExceptionInterface $e) {
+                $error = $e->getMessage();
+            }
+        }
+
+        $this->getDoctrine()->getManager()->persist($comment);
+        $this->getDoctrine()->getManager()->flush();
+        return $this->redirectToRoute('agent_ticket', ['id' => $ticket->getId()]);
+    }
+
+    /**
+     * @Route("/agent/ticket/close/{id}", name="agent_ticket_close", methods={"POST"})
+     */
+    public function closeTicket(Ticket $ticket, MailerInterface $mailer)
+    {
+        foreach ($ticket->getComments() as $comment) {
+            if ($comment->getUser() instanceof Agent && !$comment->getIsPrivate()) {
+                $ticket->setStatus(Ticket::CLOSE)
+                    ->setUpdatedDate(new \DateTimeImmutable());
+                $this->getDoctrine()->getManager()->persist($ticket);
+                $this->getDoctrine()->getManager()->flush();
+
+                $email = (new Email())
+                    ->from('becode@test.com')
+                    ->to('samihuyen059@gmail.com')
+                    ->subject('Your ticket has been closed')
+                    ->text('Your ticket is handled by one of our agent. The ticket is now closed.');
+                try {
+                    $mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+                    $error = $e->getMessage();
+                }
+
+                return $this->redirectToRoute('agent_ticket', ['id' => $ticket->getId()]);
+            }
+        }
+
+        $error = "A ticket can only be closed when it has at least one public comment from an agent.";
+        return $this->redirectToRoute('agent_ticket', ['id' => $ticket->getId()]);
+    }
+
+    /**
+     * @Route("/agent/ticket/escalate/{id}", name="agent_ticket_escalate", methods={"POST"})
+     */
+    public function escalate(Ticket $ticket, MailerInterface $mailer)
+    {
+        $ticket->setIsEscalated(true)
+            ->setAgent(null)
+            ->setStatus(Ticket::OPEN)
+            ->setUpdatedDate(new \DateTimeImmutable());
+        $this->getDoctrine()->getManager()->persist($ticket);
+        $this->getDoctrine()->getManager()->flush();
+
+        $email = (new Email())
+            ->from('becode@test.com')
+            ->to('samihuyen059@gmail.com')
+            ->subject('Your ticket is escalated')
+            ->text('Your ticket will be handled by one of our second line agent.');
+        try {
+            $mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            $error = $e->getMessage();
+        }
+
+        return $this->redirectToRoute('agent_home');
     }
 }
